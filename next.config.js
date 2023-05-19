e3cd0f2236897c9f9b494fb6a589e79768669c70
Zvezke <superur@gmail.com>
2023-05-20 01:02:08 +0200

    Changed next config

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    domains: ["treenighedskirkensdrengekor.dk", "localhost"],
+  },
   // redirects: async () => {
   //   return [
   //     {
