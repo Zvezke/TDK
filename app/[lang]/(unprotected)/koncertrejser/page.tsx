2f2302377bbd43c6047e5412fb05d9c16bb5d16a
Zvezke <superur@gmail.com>
2023-10-17 22:57:21 +0200

    Muted an erroneous component ('Koncertrejser')

--- app/[lang]/(unprotected)/koncertrejser/page.tsx
+++ app/[lang]/(unprotected)/koncertrejser/page.tsx
@@ -9,33 +9,30 @@ import PocketBase from "pocketbase";
 const pb = new PocketBase("https://pocketbase-development.up.railway.app");
 
 const Page = () => {
-  const [data, setData] = useState([]);
-
-  const displayData = async () => {
-    // Fetch all records at once via getFullList
-    const records = await pb.collection("concert_tours_json").getFullList({
-      sort: "-created",
-    });
-    console.log("Koncertrejser, records", records);
-    setData(records);
-  };
-
-  return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <Tiptap />
-      <button onClick={displayData}>Vis data</button>
-
-      <div>
-        {/* Showing data from PocketBase */}
-        {data.map((item) => (
-          <div key={item.id}>
-            <h2>{item.titel}</h2>
-            <div dangerouslySetInnerHTML={{ __html: item.text }} />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  // const [data, setData] = useState([]);
+  // const displayData = async () => {
+  //   // Fetch all records at once via getFullList
+  //   const records = await pb.collection("concert_tours").getFullList({
+  //     sort: "-created",
+  //   });
+  //   console.log("Koncertrejser, records", records);
+  //   setData(records);
+  // };
+  // return (
+  //   <div className="flex h-screen flex-col items-center justify-center">
+  //     <Tiptap />
+  //     <button onClick={displayData}>Vis data</button>
+  //     <div>
+  //       {/* Showing data from PocketBase */}
+  //       {data.map((item) => (
+  //         <div key={item.id}>
+  //           <h2>{item.titel}</h2>
+  //           <div dangerouslySetInnerHTML={{ __html: item.text }} />
+  //         </div>
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Page;
