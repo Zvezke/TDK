14929b7dae1c4e500a6b6902305b4a1475ba2e07
Zvezke <superur@gmail.com>
2023-10-17 15:16:09 +0200

    Added Tiptap (following 'Installation' guide)

---
+++ app/[lang]/(unprotected)/koncertrejser/page.tsx
@@ -0,0 +1,13 @@
+import Tiptap from "../../components/tiptap";
+
+const Page = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      {/* <h1>Koncertrejser</h1>
+      <p>This is a public page</p> */}
+      <Tiptap />
+    </div>
+  );
+};
+
+export default Page;
