d29c74a637bae92b0b19382f2e66fb15e3c7cab3
Zvezke <superur@gmail.com>
2023-12-26 02:17:31 +0100

    Add event creation and deletion functionality

--- app/[lang]/(protected)/intra/external/page.tsx
+++ app/[lang]/(protected)/intra/external/page.tsx
@@ -9,28 +9,6 @@ import { Suspense } from "react";
 // Supabase
 import { useGetAllEvents } from "./server-actions";
 
-// export default async function Page() {
-
-// console.log("supabase, data", data);
-// console.log("supabase, error", error);
-// console.log("supabase, session", supabase.auth.getSession());
-
-// React
-// import { useEffect } from "react";
-
-// Next
-// import { usePathname, redirect } from "next/navigation";
-// import { useRouter } from "next/navigation";
-
-// Types and interfaces
-// import { AuthRecord, IAuthStore, NavbarProps } from "@/types/interfaces";
-
-// Pocketbase
-// import pb from "@/pocketbase/config";
-
-// Supabase
-// import { createSupabaseFrontendClient } from "@/supabase/frontendClient";
-
 // Context
 // import { useAuth } from "@/context/AuthContext";
 
@@ -43,49 +21,11 @@ export default async function Page() {
   const { events, eventsError } = await useGetAllEvents();
   const { data } = await useServerAuthSession();
   // const { data, error } = await supabase.auth.getSession();
-  // console.log("events", events);
-  // console.log("eventsError", eventsError);
-  // const { isLoggedIn, setIsLoggedIn } = useAuth();
-  // const supabase = createSupabaseFrontendClient();
-
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   const fetchSession = async () => {
-  //     const { data, error } = await supabase.auth.getSession();
-  //     if (data && data.session) {
-  //       setIsLoggedIn(true);
-  //     } else {
-  //       setIsLoggedIn(false);
-  //       router.push("/login");
-  //     }
-  //   };
-  //   fetchSession();
-  // }, []);
-
-  // if (error) {
-  //   // console.error("Error logging in:", error);
-  // }
+  console.log("events", events);
 
   if (!data.session) {
     return redirect("/login");
   }
-
-  // const handleLogout = async () => {
-  //   const { error } = await supabase.auth.signOut();
-  //   setIsLoggedIn(false);
-  //   router.push("/login");
-  // };
-
-  // if (!isLoggedIn) {
-  //   // console.log("External, isLoggedIn", isLoggedIn);
-  //   // router.push("/login");
-  //   return <div>Ikke logget ind</div>;
-  // }
-
-  // (console.log("External, isLoggedIn", isLoggedIn),
-  //   return null;
-  // }
 
   return (
     <>
@@ -100,7 +40,7 @@ export default async function Page() {
           <div>
             <h2 className="mb-4 text-2xl">Begivenhederne</h2>
             <Suspense fallback={<div>Henter begivenheder ...</div>}>
-              <Events />
+              <Events events={events} />
             </Suspense>
           </div>
           {/* <button onClick={handleLogout}>Log ud</button> */}
--- app/[lang]/(protected)/intra/external/server-actions.ts
+++ app/[lang]/(protected)/intra/external/server-actions.ts
@@ -1,5 +1,8 @@
 "use server";
 
+// NextJS
+import { revalidatePath } from "next/cache";
+
 // Supabase
 import { createSupabaseServerComponentClient } from "@/supabase/backendClient";
 
@@ -7,13 +10,51 @@ import { createSupabaseServerComponentClient } from "@/supabase/backendClient";
 // GET //
 /////////
 
-// Get all users
+// Get all events
 const useGetAllEvents = async () => {
   const supabase = createSupabaseServerComponentClient<Database>();
   let { data: events, error: eventsError } = await supabase
     .from("events")
-    .select("*");
+    .select("*")
+    .order("date", { ascending: true });
   return { events, eventsError };
 };
 
-export { useGetAllEvents };
+////////////
+// CREATE //
+////////////
+
+// Create event
+
+interface Event {
+  body: string | null;
+  date: string | null;
+  title: string | null;
+}
+
+const useCreateEvent = async (eventData: Event) => {
+  const supabase = createSupabaseServerComponentClient<Database>();
+  let { data: event, error: eventError } = await supabase
+    .from("events")
+    .insert([eventData])
+    .select("*");
+  revalidatePath("/[lang]/(protected)/intra/external/components/Events");
+  return { event, eventError };
+};
+
+//////////////////
+// Delete event //
+//////////////////
+
+// Delete event
+const useDeleteEvent = async (eventId: string) => {
+  const supabase = createSupabaseServerComponentClient<Database>();
+  const { error: deleteEventError } = await supabase
+    .from("events")
+    .delete()
+    .eq("id", eventId);
+  revalidatePath("/[lang]/(protected)/intra/external/components/Events");
+  return { deleteEventError };
+};
+
+export { useGetAllEvents, useCreateEvent, useDeleteEvent };
