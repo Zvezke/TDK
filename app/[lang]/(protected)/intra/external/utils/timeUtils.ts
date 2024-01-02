505430ba040c0b4c506dae20a295ebc906721660
Zvezke <superur@gmail.com>
2024-01-02 18:08:43 +0100

    Update date formatting in Events component

--- app/[lang]/(protected)/intra/external/utils/timeUtils.ts
+++ app/[lang]/(protected)/intra/external/utils/timeUtils.ts
@@ -70,50 +70,65 @@ export function createDateTimeForSupabase(
     monthNumber - 1,
     parseInt(day)
   );
-  const isDst = isDaylightSavingTime(dateForDstCheck);
+  // const isDst = isDaylightSavingTime(dateForDstCheck);
 
-  const newTimeString = adjustForDaylightSaving(daytime, isDst);
+  // console.log("dateForDstCheck", dateForDstCheck);
+
+  // Get the timezone offset in minutes.
+  // const offset = dateForDstCheck.getTimezoneOffset();
+  // Convert the offset to hours.
+  // const offsetHours = offset / 60;
+
+  // Subtract the offset from the input time.
+  // const adjustedTime = subtractedHours(daytime, offsetHours);
+
+  // console.log("adjustedTime", adjustedTime);
+
+  // const newTimeString = adjustForDaylightSaving(daytime, isDst);
   const formattedDateString = addLeadingZeroToDate(
     `${year}-${monthNumber}-${day}`
   );
-  return `${formattedDateString} ${newTimeString}:00.000Z`;
+  console.log("formattedDateString", formattedDateString);
+  console.log("daytime", daytime);
+  return `${formattedDateString} ${daytime}:00.000Z`;
+  // return `${formattedDateString} ${newTimeString}:00.000Z`;
 }
 
-/**
- * Calculates the last Sunday of a given month in a given year.
- * @param month - The month (1-12).
- * @param year - The year.
- * @returns The date of the last Sunday of the specified month.
- */
-export function getLastSunday(month: number, year: number): Date {
-  const date = new Date(year, month, 0); // Last day of the month
-  date.setDate(date.getDate() - date.getDay()); // Adjust to the previous Sunday
-  return date;
-}
+// /**
+//  * Calculates the last Sunday of a given month in a given year.
+//  * @param month - The month (1-12).
+//  * @param year - The year.
+//  * @returns The date of the last Sunday of the specified month.
+//  */
+// export function getLastSunday(month: number, year: number): Date {
+//   const date = new Date(year, month, 0); // Last day of the month
+//   date.setDate(date.getDate() - date.getDay()); // Adjust to the previous Sunday
+//   return date;
+// }
 
-/**
- * Checks if a given date is in daylight saving time.
- * @param date - The date to check.
- * @returns True if the date is in daylight saving time, false otherwise.
- */
-export function isDaylightSavingTime(date: Date): boolean {
-  const year = date.getFullYear();
-  const lastSundayMarch = getLastSunday(3, year);
-  const lastSundayOctober = getLastSunday(10, year);
+// /**
+//  * Checks if a given date is in daylight saving time.
+//  * @param date - The date to check.
+//  * @returns True if the date is in daylight saving time, false otherwise.
+//  */
+// export function isDaylightSavingTime(date: Date): boolean {
+//   const year = date.getFullYear();
+//   const lastSundayMarch = getLastSunday(3, year);
+//   const lastSundayOctober = getLastSunday(10, year);
 
-  return date > lastSundayMarch && date < lastSundayOctober;
-}
+//   return date > lastSundayMarch && date < lastSundayOctober;
+// }
 
-/**
- * Adjusts a time string for daylight saving time.
- * @param timeString - The time string in the format "HH:mm".
- * @param isDst - A boolean indicating whether daylight saving time is in effect.
- * @returns The adjusted time string.
- */
-export function adjustForDaylightSaving(
-  timeString: string,
-  isDst: boolean
-): string {
-  const hoursToAdjust = isDst ? 0 : 1; // Adjust by 1 hour for DST, otherwise 2
-  return subtractedHours(timeString, hoursToAdjust);
-}
+// /**
+//  * Adjusts a time string for daylight saving time.
+//  * @param timeString - The time string in the format "HH:mm".
+//  * @param isDst - A boolean indicating whether daylight saving time is in effect.
+//  * @returns The adjusted time string.
+//  */
+// export function adjustForDaylightSaving(
+//   timeString: string,
+//   isDst: boolean
+// ): string {
+//   const hoursToAdjust = isDst ? 0 : 1; // Adjust by 1 hour for DST, otherwise 2
+//   return subtractedHours(timeString, hoursToAdjust);
+// }
