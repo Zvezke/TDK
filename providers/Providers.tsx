6deec3eeb4319af2582109cb1582a2a4fc6e297a
Zvezke <superur@gmail.com>
2024-03-12 18:02:32 +0100

    Add next-themes support to Providers component

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,7 +1,16 @@
 "use client";
 
+// export function AuthProviders({ children }: { children: React.ReactNode }) {
+//   return <AuthProvider>{children}</AuthProvider>;
+// }
+
+import { ThemeProvider } from "next-themes";
 import { AuthProvider } from "@/context/AuthContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <AuthProvider>{children}</AuthProvider>
+    </ThemeProvider>
+  );
 }
