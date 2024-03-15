a81cbcf4361e33830d0d24758f8cc5483c9f100e
Zvezke <superur@gmail.com>
2024-03-15 10:18:15 +0100

    Refactor locale-switcher and navbar components

--- middleware.ts
+++ middleware.ts
@@ -49,3 +49,63 @@ export const config = {
   // Matcher ignoring `/_next/` and `/api/`
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { cookies } from "next/headers";
+
+// import { i18n } from "./i18n-config";
+
+// import { match as matchLocale } from "@formatjs/intl-localematcher";
+// import Negotiator from "negotiator";
+
+// function getLocale(request: NextRequest): string | undefined {
+//   const cookieStore = cookies();
+//   const nextLocale = cookieStore.get("NEXT_LOCALE");
+//   // console.log(nextLocale);
+//   // const nextLocale = cookies["NEXT_LOCALE"];
+
+//   if (
+//     typeof nextLocale === "string" &&
+//     (nextLocale === "en" || nextLocale === "da")
+//   ) {
+//     return nextLocale;
+//   }
+
+//   // Negotiator expects plain object so we need to transform headers
+//   const negotiatorHeaders: Record<string, string> = {};
+//   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
+
+//   // Use negotiator and intl-localematcher to get best locale
+//   let languages = new Negotiator({ headers: negotiatorHeaders }).languages();
+//   // @ts-ignore locales are readonly
+//   const locales: string[] = i18n.locales;
+//   return matchLocale(languages, locales, i18n.defaultLocale);
+// }
+
+// export function middleware(request: NextRequest) {
+//   const pathname = request.nextUrl.pathname;
+//   // Define a regex pattern to match paths that should be ignored
+//   const ignorePattern =
+//     /^\/(manifest.json|images\/.*|your-other-patterns-here)/;
+
+//   // Check if the pathname matches the ignore pattern
+//   if (ignorePattern.test(pathname)) {
+//     // If it matches, do nothing and return
+//     return;
+//   }
+//   // Check if there is any supported locale in the pathname
+//   const pathnameIsMissingLocale = i18n.locales.every(
+//     (locale) =>
+//       !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+//   );
+//   // Redirect if there is no locale
+//   if (pathnameIsMissingLocale) {
+//     const locale = getLocale(request);
+//     // e.g. incoming request is /products
+//     // The new URL is now /en-US/products
+//     return NextResponse.redirect(
+//       new URL(`/${locale}/${pathname}`, request.url),
+//     );
+//   }
+// }
